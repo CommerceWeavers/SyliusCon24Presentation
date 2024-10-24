62f7da85cf03da775b6216c4a7a1637bc95f1ad1
Łukasz Chruściel <lchrusciel@gmail.com>
2024-10-25 00:21:29 +0200

    Define phase five

---
+++ src/Feed/PhaseFive/Handler/FeedForProductsGeneratedHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Feed\PhaseFive\Handler;
+
+use App\Entity\Channel\ChannelPricing;
+use App\Entity\Product\Product;
+use App\Entity\Product\ProductVariant;
+use App\Feed\Model\Element;
+use App\Feed\PhaseFive\Command\GenerateFeedForProducts;
+use App\Feed\PhaseFive\Event\FeedForProductsGenerated;
+use App\Feed\Repository\ProductRepository;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#[AsMessageHandler]
+final readonly class FeedForProductsGeneratedHandler
+{
+    public function __construct(
+        #[Autowire(param: 'kernel.project_dir')]
+        private string $projectDir,
+    ) {
+    }
+
+    public function __invoke(FeedForProductsGenerated $feedForProductsGenerated): void
+    {
+        file_put_contents($this->projectDir . '/private/phase-five.txt', json_encode($feedForProductsGenerated->productFeeds));
+    }
+}
